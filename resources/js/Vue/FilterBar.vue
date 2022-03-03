6e07a7a14c76f6d023e99277624372a1bedca264
Bogdan-Cristian Ciumac <bogdan.ciumac@check24.de>
2022-03-03 20:03:22 +0100

    added filterbar

---
+++ resources/js/Vue/FilterBar.vue
@@ -0,0 +1,55 @@
+<template>
+    <div class="filter_bar_wrapper">
+        <div class="wrapper__box" @click="filterContent()">
+            <p class="box__text">Popularity</p>
+        </div>
+
+        <div class="wrapper__box" @click="filterContent()">
+            <p class="box__text">ASC</p>
+        </div>
+
+        <div class="wrapper__box" @click="filterContent()">
+            <p class="box__text">DESC</p>
+        </div>
+    </div>
+</template>
+
+<script>
+export default {
+    name: "filter-bar",
+    methods: {
+        filterContent() {
+        }
+    }
+}
+</script>
+
+<style lang="less" scoped>
+    @bg-color: #303088;
+    @text-color: white;
+
+    .filter_bar_wrapper {
+        display: flex;
+        padding:10px 0;
+
+        .wrapper__box {
+            background-color: @bg-color;
+            color:@text-color;
+            font-size: 12px;
+            border-radius: 12px;
+            padding: 5px 7px;
+            margin-left: 20px;
+            cursor: pointer;
+
+            &:first-child {
+                margin-left: 0;
+            }
+
+            .box__text {
+                padding: 0;
+                margin: 0;
+            }
+        }
+
+    }
+</style>
